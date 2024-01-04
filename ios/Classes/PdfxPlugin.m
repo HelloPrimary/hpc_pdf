06d72dd0e5688bae058e149752dd85f640139161
Thomas Pucci <thomas.lb.pucci@gmail.com>
2024-01-04 18:32:46 +0100

    fix: fix plugin

--- ios/Classes/PdfxPlugin.m
+++ ios/Classes/PdfxPlugin.m
@@ -5,7 +5,7 @@
 // Support project import fallback if the generated compatibility header
 // is not copied when this plugin is created as a library.
 // https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "pdfx-Swift.h"
+#import "hpc_pdf-Swift.h"
 #endif
 
 @implementation PdfxPlugin
